933bf43d603274f4cfd18ebfbfc49088e8753f25
Nora Vargas <noravgs92@gmail.com>
2022-11-29 17:39:32 -0600

    updated home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,7 @@ const Home = () => {
   <div name='home' className='w-full h-screen bg-[#072a57]'>
    {/*container*/}
    <div className="flex flex-col justify-center px-6 py-3 h-screen bg-main w-full bg-center bg-cover">
-    {/* <p className='text-[#ade1e5]'>WHERE GRIT MEETS OPPORTUNITY</p> */}
     <h1 className='text-4xl sm:text-7xl font:bold text-white'>WHERE GRIT MEETS OPPORTUNITY</h1>
-    {/* <h2 className='text-4xl sm:text-7xl font:bold text-[#8f8e8e]'>Full Stack Software Engineer</h2> */}
     <h2 className='text-[#9a9b9b] py-4 max-w[700px]'>Fitness has no finish line Workout with us</h2>
     <div>
       <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#FF6973] hover:text-black hover:border-[#ffffff]'>View Classes <HiArrowNarrowRight className='ml-3 group-hover:rotate-90 duration-300' /></button>
